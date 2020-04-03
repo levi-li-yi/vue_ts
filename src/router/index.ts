0109e44a8b91c956d4063c247da99ed8e52cf251
liyi <liyi@sinux.com>
2020-04-03 16:15:30 +0800

    add apge file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
@@ -9,14 +8,6 @@ const routes = [
     path: '/',
     name: 'Layout',
     component: () => import(/* webpackChunkName: "layout" */ '../views/layout/Layout.vue'),
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
 ];
 
